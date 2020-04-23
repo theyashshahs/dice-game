95d810b7c890558f650956cace95b8e616a9dc2f
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-04-24 00:05:04 +0530

    added toggle for changing player

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ scores = [0, 0];
 roundScore = 0;
 activePLayer = 0;
 
-document.querySelector('#current-' + activePLayer).textContent = dice;
+
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
@@ -27,4 +32,24 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
+
+    //3. Update the scores
+    if(dice !== 1)
+    {   
+        roundScore += dice;
+        document.querySelector("#current-" + activePLayer).textContent = roundScore;
+    }
+
+    else{
+        activePLayer === 0 ? activePLayer = 1 : activePLayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector(".player-0-panel").classList.toggle("active");
+		document.querySelector(".player-1-panel").classList.toggle("active");
+    }
 });
