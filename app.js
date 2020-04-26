b47d2d871991955edf83f0e500722fd539b0e9f4
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-04-26 17:17:48 +0530

    global score and winner check function added :heavy_check_mark:

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -41,15 +41,47 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     }
 
     else{
-        activePLayer === 0 ? activePLayer = 1 : activePLayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-        document.querySelector(".player-0-panel").classList.toggle("active");
-		document.querySelector(".player-1-panel").classList.toggle("active");
+        // next player
+        nextPlayer();
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+
+    // Add current score to the global score score
+    scores[activePLayer] += roundScore;  
+
+    // Update the UI
+    document.querySelector('#score-' + activePLayer).textContent = scores[activePLayer];
+
+    
+    // Check if player won the game
+    if(scores[activePLayer] >= 100) {
+        document.querySelector('#name-' + activePLayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+    else {
+        // next player
+        nextPlayer();
+	}
+
+    
+});
+
+function nextPlayer() {
+    
+    activePLayer === 0 ? (activePLayer = 1) : (activePLayer = 0);
+	roundScore = 0;
+
+	document.getElementById("current-0").textContent = "0";
+	document.getElementById("current-1").textContent = "0";
+
+	// document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    
+	document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    document.querySelector('.dice').style.display = 'none';
+}
